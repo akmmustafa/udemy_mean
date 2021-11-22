df978b9fec2403f46b9b8e7ea83b314541d0f786
Mohamed Mustafa <mohamedmustafa.mak@gmail.com>
2021-11-22 22:50:55 +0530

    Adding Node and Express
    Adding Node and Express

---
+++ backend/app.js
@@ -0,0 +1,15 @@
+//Holds all belongs to express framework
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next)=>{
+  console.log('First Middleware');
+  next();
+});
+
+app.use((req, res, next)=>{
+  res.send('Hello from express!');
+});
+
+module.exports = app;
