cbe31f18a3513f22960c3d231e957e133e493a28
Mohamed Mustafa <mohamedmustafa.mak@gmail.com>
2021-11-22 21:18:51 +0530

    Update app.module.ts
    adding Material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,8 +11,8 @@ import {
 } from "@angular/material";
 
 import { AppComponent } from "./app.component";
-import { PostCreateComponent } from "./posts/post-create/post-create.component";
 import { HeaderComponent } from "./header/header.component";
+import { PostCreateComponent } from "./posts/post-create/post-create.component";
 import { PostListComponent } from "./posts/post-list/post-list.component";
 
 @NgModule({
