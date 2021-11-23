9352dd56fac1a5cb03ea6b6e75a27b01407a9f36
Mohamed Mustafa <mohamedmustafa.mak@gmail.com>
2021-11-23 16:47:48 +0530

    node and express implementations completed
    node and express implementations completed

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,44 @@
 //Holds all belongs to express framework
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use((req, res, next)=>{
-  console.log('First Middleware');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
   next();
 });
-
-app.use((req, res, next)=>{
-  res.send('Hello from express!');
+app.post('/api/posts', (req, res, next)=>{
+  const post = req.body;
+  console.log(post);
+  res
+    .status(201)
+    .json({
+      message: 'Post added successfully'
+    });
+});
+app.get('/api/posts', (req, res, next)=>{
+  const posts = [
+    {
+      id: 'A11111111',
+      title: 'First Title',
+      content: 'First Content from server'
+    },
+    {
+      id: 'B22222222',
+      title: 'Second Title',
+      content: 'Second Content from server'
+    }
+  ];
+  res
+    .status(200)
+    .json({
+      message: 'Post fetched Successfully !',
+      posts: posts
+    });
 });
 
 module.exports = app;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 import {
   MatInputModule,
   MatCardModule,
@@ -25,6 +26,7 @@ import { PostListComponent } from "./posts/post-list/post-list.component";
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
     BrowserAnimationsModule,
     MatInputModule,
     MatCardModule,
